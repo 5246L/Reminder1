ae3b266a61c8ac2953f28193c73cee74cd0d7d40
kalininlev <levakalinin39@gmail.com>
2026-02-10 07:37:39 +0300

    commit4

--- src/main/java/org/example/reminder1/service/UserService.java
+++ src/main/java/org/example/reminder1/service/UserService.java
@@ -45,8 +45,8 @@ public class UserService {
     }
 */
 
-    public User createUser(User user) {
-        return userRepository.save(user);
+    public void createUser(User user) {
+        userRepository.save(user);
     }
 
     public User getUserByGoogleId(String googleId) {
