85135a2d4494081585428dd3cd2a78f552c07784
kalininlev <levakalinin39@gmail.com>
2026-02-14 08:11:42 +0300

    newcommit2

--- src/main/java/org/example/reminder1/service/TelegramService.java
+++ src/main/java/org/example/reminder1/service/TelegramService.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+
 @Slf4j
 @Service
 @RequiredArgsConstructor
@@ -14,17 +17,19 @@ public class TelegramService {
     @Value("${telegram.bot.token}")
     private String botToken;
 
-    private final RestTemplate restTemplate = new RestTemplate();
+    private final RestTemplate restTemplate;
 
     public void sendReminder(Long chatId, String title, String description) {
         try {
+            String text = "🔔 " + title + "\n\n" + description;
+            String encodedText = URLEncoder.encode(text, StandardCharsets.UTF_8);
+
             String url = String.format(
                     "https://api.telegram.org/bot%s/sendMessage?chat_id=%d&text=%s",
                     botToken,
                     chatId,
-                    "🔔 " + title + "\n\n" + description
+                    encodedText  // ← Закодированный текст
             );
-
             restTemplate.getForObject(url, String.class);
 
             log.info("Успешно отправлено на Telegram {}", chatId);
@@ -33,5 +38,14 @@ public class TelegramService {
             log.error("Ошибка отправки Telegram: {}", e.getMessage());
             throw new RuntimeException("Не удалось отправить Telegram", e);
         }
+    }
+
+    public String getUpdates() {
+        String url = String.format(
+          "https://api.telegram.org/bot%s/getUpdates",
+          botToken
+        );
+
+        return restTemplate.getForObject(url, String.class);
     }
 }
