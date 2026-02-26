0394b5a70a3341a160ed49f256027450a8e662b8
kalininlev <levakalinin39@gmail.com>
2026-02-26 11:22:15 +0300

    fix config plus authController

--- src/main/java/org/example/reminder1/controller/AuthController.java
+++ src/main/java/org/example/reminder1/controller/AuthController.java
@@ -2,13 +2,13 @@ package org.example.reminder1.controller;
 
 
 import lombok.RequiredArgsConstructor;
+import org.example.reminder1.dto.AuthSuccessResponse;
+import org.example.reminder1.dto.CurrentUserResponse;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/auth")
@@ -16,20 +16,20 @@ import java.util.Map;
 public class AuthController {
 
     @GetMapping("/success")
-    public Map<String, Object> loginSuccess(@AuthenticationPrincipal OAuth2User oauth2User) {
-        return Map.of(
-                "message", "Авторизация успешна!",
-                "name", oauth2User.getAttribute("name"),
-                "email", oauth2User.getAttribute("email")
+    public AuthSuccessResponse loginSuccess(@AuthenticationPrincipal OAuth2User oauth2User) {
+        return new AuthSuccessResponse(
+                "Авторизация успешна!",
+                oauth2User.getAttribute("name"),
+                oauth2User.getAttribute("email")
         );
     }
 
     @GetMapping("/me")
-    public Map<String, Object> getCurrentUser(@AuthenticationPrincipal OAuth2User oauth2User) {
-        return Map.of(
-                "name", oauth2User.getAttribute("name"),
-                "email", oauth2User.getAttribute("email"),
-                "picture", oauth2User.getAttribute("picture")
+    public CurrentUserResponse getCurrentUser(@AuthenticationPrincipal OAuth2User oauth2User) {
+        return new CurrentUserResponse(
+                oauth2User.getAttribute("name"),
+                oauth2User.getAttribute("email"),
+                oauth2User.getAttribute("picture")
         );
     }
 }
